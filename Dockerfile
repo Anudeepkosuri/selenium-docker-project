251346219cc57b56e8c7a3ffda17858e34b8e5f4
anudeepkosuri <anudeepkosuri123@gmsil.com>
2026-03-01 15:31:14 +0530

    hello

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,16 @@
 FROM maven:3.9.6-eclipse-temurin-17
 
 # Install required packages
-FROM maven:3.9.6-eclipse-temurin-17
-
 RUN apt-get update && apt-get install -y \
     wget \
     curl \
     unzip \
-
+    gnupg \
+    xvfb
 
 # Install Google Chrome
 RUN wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
-    && apt-get install -y ./google-chrome-stable_current_amd64.deb \
+    && apt-get install -y ./google-chrome-stable_current_adev64.deb \
     || apt --fix-broken install -y
 
 # Set working directory inside container
@@ -21,5 +20,5 @@ WORKDIR /app
 # Copy project files into container
 COPY . .
 
-# Run Maven tests
-CMD ["mvn", "clean", "test"]
+# Run Maven tests for SelectDateTest
+CMD ["mvn", "clean", "test", "-Dtest=SelectDateTest"]
--- src/test/java/SelectDateTest.java
+++ src/test/java/SelectDateTest.java
@@ -2,21 +2,32 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
 
 public class SelectDateTest {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
 
     @Test
     public void selectDateTest() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         driver.get("https://testautomationpractice.blogspot.com/");
@@ -24,7 +35,7 @@ public class SelectDateTest {
     }
 
     @AfterTest
-    public void tearDown(WebDriver driver) {
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
